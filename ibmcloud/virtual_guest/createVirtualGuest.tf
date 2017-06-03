f268bf3efbff186b0daa215af13ef8deda21c7d5
santosh_pawar <psantosh@us.ibm.com>
2017-06-03 15:56:00 +0530

    Redirect errors to log file

--- ibmcloud/virtual_guest/createVirtualGuest.tf
+++ ibmcloud/virtual_guest/createVirtualGuest.tf
@@ -56,7 +56,7 @@ resource "ibmcloud_infra_virtual_guest" "softlayer_virtual_guest" {
     inline = [
       "ls -al",
       "chmod +x install.sh",
-      "bash install.sh > /var/log/mdm-install.log",
+      "bash install.sh 2>&1 | tee /var/log/mdm-install.log",
       "bash -c 'if [ \"${var.remove_temp_private_key}\" == \"true\" ] ; then KEY=$(echo \"${var.temp_public_key}\" | cut -c 9-); cd /root/.ssh; grep -v $KEY authorized_keys > authorized_keys.new; mv -f authorized_keys.new authorized_keys; chmod 600 authorized_keys; fi'",
       "${var.module_custom_commands}"
     ]

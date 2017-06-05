c68f1cce08f2600940cb781c065e28066eb05562
santosh_pawar <psantosh@us.ibm.com>
2017-06-05 18:05:45 +0530

    Removed log message

--- ibmcloud/virtual_guest/files/install.sh
+++ ibmcloud/virtual_guest/files/install.sh
@@ -156,4 +156,3 @@ cd $TOP/bin/go
 echo "***** MDMCE installation finished *****"
 
 echo "MDM Installation finished at `date`"
-echo "MDM started successfully, you can access it at http://`hostname -i`:7507/utils/enterLogin.jsp"

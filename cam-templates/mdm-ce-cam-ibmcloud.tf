36f09e328bc50e9c7a4282ead54e6e139a0aa9af
santosh_pawar <psantosh@us.ibm.com>
2017-06-06 09:33:05 +0530

    Initial checkin

---
+++ cam-templates/mdm-ce-cam-ibmcloud.tf
@@ -0,0 +1,57 @@
+{
+    "provider": {
+        "ibmcloud": {
+        }
+    },
+    "variable": {
+		"customer": {
+            "description": "Name of the customer (Single word, all letters in small)"
+        },
+		"datacenter": {
+            "description": "Softlayer datacenter where infrastructure resources will be deployed",
+			"default": "dal09"
+        },
+        "public_ssh_key": {
+            "description": "Public SSH key used to connect to the virtual guest"
+        }
+    },
+    "resource": {
+        "tls_private_key": {
+            "ssh": {
+                "algorithm": "RSA"
+            }
+        },
+        "ibmcloud_infra_ssh_key": {
+            "cam_public_key": {
+                "label": "CAM Public Key",
+                "public_key": "${var.public_ssh_key}"
+            },
+            "temp_public_key": {
+                "label": "Temp Public Key",
+                "public_key": "${tls_private_key.ssh.public_key_openssh}"
+            }
+        }
+    },
+	"module": {
+        "install_mdm_ibmcloud": {
+            "source": "git::https://github.com/santoshjpawar/mdm-deploy.git?ref=master//ibmcloud/virtual_guest",
+            "hostname": "mdm-node-${var.customer}",
+            "datacenter": "${var.datacenter}",
+            "user_public_key_id": "${ibmcloud_infra_ssh_key.cam_public_key.id}",
+            "temp_public_key_id": "${ibmcloud_infra_ssh_key.temp_public_key.id}",
+            "temp_public_key": "${tls_private_key.ssh.public_key_openssh}",  
+            "temp_private_key": "${tls_private_key.ssh.private_key_pem}",
+            "module_script": "files/install.sh",
+            "os_reference_code": "CENTOS_7_64",
+            "domain": "cam.ibm.com",
+            "cores": 2,
+            "memory": 2048,
+            "disk1": 100
+        }
+    },
+	"output": {
+        "You can access the MDMCE application using the following url": {
+            "value": "http://${module.install_mdm_ibmcloud.public_ip}:7507/utils/enterLogin.jsp"
+        }
+    }
+}

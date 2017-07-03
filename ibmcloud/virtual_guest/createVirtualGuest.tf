2bacb09db75b0949e4489d0304ed04ad5bd81714
santoshjpawar <santosh_pawar@persistent.com>
2017-07-03 16:45:56 +0530

    added line by master

--- ibmcloud/virtual_guest/createVirtualGuest.tf
+++ ibmcloud/virtual_guest/createVirtualGuest.tf
@@ -67,3 +67,4 @@ resource "ibmcloud_infra_virtual_guest" "softlayer_virtual_guest" {
 output "public_ip" {
   value = "${ibmcloud_infra_virtual_guest.softlayer_virtual_guest.ipv4_address}"    
 }
+# Line-master

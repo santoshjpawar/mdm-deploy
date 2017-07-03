2305db5ce2cb058ea4caa578f5246b5bc9581680
santoshjpawar <santosh_pawar@persistent.com>
2017-07-03 17:24:19 +0530

    Clean

--- ibmcloud/virtual_guest/createVirtualGuest.tf
+++ ibmcloud/virtual_guest/createVirtualGuest.tf
@@ -67,5 +67,3 @@ resource "ibmcloud_infra_virtual_guest" "softlayer_virtual_guest" {
 output "public_ip" {
   value = "${ibmcloud_infra_virtual_guest.softlayer_virtual_guest.ipv4_address}"    
 }
-# Line-master
-# Line-santosh
